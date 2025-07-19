2bbd7e41ede8d5f70c5ea3df4fb3be4d0a7a54ff
新たな旅 <142149583+inabananami@users.noreply.github.com>
2025-07-19 22:53:07 +0800

    delete extra project
    -

--- SpringCode/springboot-mybatis/src/main/java/com/itheima/springbootmybatis/service/EmpService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.springbootmybatis.service;
-
-import com.itheima.springbootmybatis.pojo.Emp;
-
-public interface EmpService {
-    public Emp getById(Integer id);
-}
--- big-event/src/main/java/com/itheima/mapper/UserMapper.java
+++ big-event/src/main/java/com/itheima/mapper/UserMapper.java
@@ -1,11 +1,18 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+@Mapper
 public interface UserMapper {
     //根据用户名查询用户
+    @Select("select * from user where username=#{username}")
     public User findByUserName(String username);
 
     //添加加密后的md5密码
-    void add(String username, String md5String);
+    @Insert("insert into user(username,password,create_time,update_time)" +
+                "values (#{username},#{password},now(),now())")
+    void add(String username, String password);
 }
